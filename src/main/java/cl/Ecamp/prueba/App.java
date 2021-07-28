ae0234df616d9564053660941e63a802526a021a
Lorena Donoso Heredia <lorena.donoso.h@gmail.com>
2021-07-27 20:09:36 -0400

    Primera foto

---
+++ src/main/java/cl/Ecamp/prueba/App.java
@@ -0,0 +1,7 @@
+package cl.Ecamp.prueba;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/cl/Ecamp/prueba/AppTest.java
@@ -0,0 +1,14 @@
+package cl.Ecamp.prueba;
+
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.junit.FixMethodOrder;
+import org.junit.Assert;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assert(true);
+  }
+}
